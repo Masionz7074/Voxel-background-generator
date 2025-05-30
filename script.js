115b09558a03ccd6260c8b8de3b0ca1f0000c576
Masionz7074 <kids.larmore22@gmail.com>
2025-05-30 14:38:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const playlist = [
+    {
+        title: "Transcendence",
+        artist: " Alexander Nakarada",
+        durationSeconds: 810,
+        audioFileName: "white_flag.mp3",
+        imageFileName: "white_flag.png"
+    },
+    {
+        title: "Sweet",
+        artist: "LiQWYD",
+        durationSeconds: 153,
+        audioFileName: "song2.mp3",
+        imageFileName: "image2.png"
+    },
+    {
+        title: "Missing You",
+        artist: "Purrple Cat",
+        durationSeconds: 227,
+        audioFileName: "song3.mp3",
+        imageFileName: "image3.png"
+    },
+    {
+        title: "Zombies",
+        artist: "xethrocc",
+        durationSeconds: 192,
+        audioFileName: "song4.mp3",
+        imageFileName: "image4.png"
+    }
+];
